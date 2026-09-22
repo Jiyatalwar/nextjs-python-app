e9f967d4382d8df4f87f2886f6e8a2d5b5e0e9a7
# 👋 Hi, I'm Jiya talwar <166608289+Jiyatalwar@users.noreply.github.com>
2026-09-22 11:56:14 +0530

    Fix comments and formatting in auth.py
    add comments for  formation details

--- backend/auth.py
+++ backend/auth.py
@@ -2,12 +2,12 @@ import secrets
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+# password create 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
-
+# verfiy password 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
-
+#  genarate token for  crete 
 def generate_reset_token() -> str:
     return secrets.token_urlsafe(32)
